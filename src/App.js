8a156105ccc7c82a8f27707e99ff28a4e7cf2210
jwright90 <jason.wright90@gmail.com>
2021-10-05 20:16:03 +0100

    Calculate winner function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import Board from "./components/Board";
 import './styles/root.scss';
+import calculateWinner from './helpers.js';
 
 const App = () => {
 
   const [board, setBoard] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(board);
+
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${isXNext ? 'X' : 'O'}`
 
   const handleSquareClick = position => {
     if (board[position]) {
@@ -28,6 +35,7 @@ const App = () => {
   return (
     <div className="app">
       <h1>Tic Tac Toe</h1>
+      <h2>{message}</h2>
       <Board board={board} handleSquareClick={handleSquareClick} />
     </div>
   );
