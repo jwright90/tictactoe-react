6f11751dd0c7a936a30af99db20da7c5d41ae134
jwright90 <jason.wright90@gmail.com>
2021-10-05 20:33:28 +0100

    add isDraw() check

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Board from "./components/Board";
 import './styles/root.scss';
-import calculateWinner from './helpers.js';
+import { calculateWinner } from './helpers';
 
 const App = () => {
 
@@ -10,12 +10,24 @@ const App = () => {
 
   const winner = calculateWinner(board);
 
+  const isDraw = () => {
+    if (!board.includes(null) && winner === null) {
+      return true;
+    }
+  }
+
   const message = winner
     ? `Winner is ${winner}`
+    : isDraw() ? "Draw"
     : `Next player is ${isXNext ? 'X' : 'O'}`
 
+  console.log('Winner:' + winner);
+  console.log('Board:' + board);
+
+  const draw = isDraw();
+  
   const handleSquareClick = position => {
-    if (board[position]) {
+    if (board[position] || winner) {
       return;
     }
 
@@ -36,6 +48,7 @@ const App = () => {
     <div className="app">
       <h1>Tic Tac Toe</h1>
       <h2>{message}</h2>
+      <h2>{draw}</h2>
       <Board board={board} handleSquareClick={handleSquareClick} />
     </div>
   );
--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-const calculateWinner = squares => {
+export const calculateWinner = squares => {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -18,4 +18,4 @@ const calculateWinner = squares => {
   return null;
 }
 
-export default calculateWinner;
+
